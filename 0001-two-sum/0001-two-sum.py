dffe16ac0b27f2e9b4917411acf32d1513ff2d8b
ZahraAbdallahMeki <zahrameki18@gmail.com>
2023-05-25 21:38:14 +0300

    Time: 70 ms (67.81%), Space: 17.7 MB (8.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+          reset = target - nums[i]
+          if reset in dic:
+            return [i, dic[reset]]
+          dic[nums[i]] = i
+          
         #=== Solution 1 ===========================         
         # for i in range(len(nums)):
         #     for j in range(i+1, len(nums)):
@@ -21,10 +28,5 @@ class Solution:
         #   if reset in dic and dic[reset] != i:
         #     return [i, dic[reset]]
         #=========================================
-        dic = {}
-        for i in range(len(nums)):
-          reset = target - nums[i]
-          if reset in dic:
-            return [i, dic[reset]]
-          dic[nums[i]] = i
+
             
