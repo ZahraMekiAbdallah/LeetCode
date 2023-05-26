e3e99efce48b369da61cc02706faa08a8e8b3dc1
ZahraAbdallahMeki <zahrameki18@gmail.com>
2023-05-26 15:42:45 +0300

    Time: 59 ms (65.13%), Space: 16.6 MB (12.67%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -8,41 +8,35 @@ class Solution:
       res = [-1] * len(nums1)
       stack = []
       
-      for i in range(len(nums2)-1, -1, -1):
+      for i in range(len(nums2)):
+        while stack and nums2[i] > stack[-1]:
+          last_el = stack.pop() 
+          res[dic[last_el]] = nums2[i]
         if nums2[i] in dic.keys():
-          while stack:
-            last_el = stack.pop()
-            if last_el > nums2[i]: 
-              res[dic[nums2[i]]] = last_el
-              stack.append(last_el)
-              break
-        stack.append(nums2[i])
-          
+          stack.append(nums2[i])  
       return res
         
 
 #  Reversed array 
-#       dic = {}
-#       for i in range(len(nums1)):
-#         dic[nums1[i]] = i
-        
-#       res = [-1] * len(nums1)
-#       stack = []
-      
-#       for i in range(len(nums2)-1, -1, -1):
-#         if nums2[i] in dic.keys():
-#           while stack:
-#             last_el = stack.pop()
-#             if last_el > nums2[i]: 
-#               res[dic[nums2[i]]] = last_el
-#               stack.append(last_el)
-#               break
-#           stack.append(nums2[i])
-#         else:
-#           stack.append(nums2[i])
-          
-#       return res
-        
+#     dic = {}
+#     for i in range(len(nums1)):
+#       dic[nums1[i]] = i
+
+#     res = [-1] * len(nums1)
+#     stack = []
+
+#     for i in range(len(nums2)-1, -1, -1):
+#       if nums2[i] in dic.keys():
+#         while stack:
+#           last_el = stack.pop()
+#           if last_el > nums2[i]: 
+#             res[dic[nums2[i]]] = last_el
+#             stack.append(last_el)
+#             break
+#       stack.append(nums2[i])
+
+#     return res
+
     
     
     
