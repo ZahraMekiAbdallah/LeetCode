989a70def27afd13cc866cf48cd52a7decaf2111
ZahraAbdallahMeki <zahrameki18@gmail.com>
2023-05-26 19:00:46 +0300

    Time: 39 ms (57.18%), Space: 16.7 MB (35.71%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+      ''' Left leaf DFS''' 
+      q = deque([[root, 0]])
+      res = 0
+      while q:
+        node, direction = q.popleft()
+        if not node.left and not node.right and direction == 1:
+          res+=node.val
+        if node.left:
+          q.append([node.left, 1])
+        if node.right:
+          q.append([node.right, 0])
+      return res
+
+            
