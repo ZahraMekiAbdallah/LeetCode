964bcdf08e1bb3d9047d78ebf25d00fe68995a55
ZahraAbdallahMeki <zahrameki18@gmail.com>
2023-05-26 18:20:55 +0300

    Time: 57 ms (8.15%), Space: 16.3 MB (33.26%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
@@ -6,16 +6,29 @@ class Solution:
         return True
       
       q = deque([root])
+      nullFound = False
       
-      while q and q[0] is not None: # add children for all not none nodes
+      while q:
         node = q.popleft()
-        q.append(node.left)
-        q.append(node.right)
+        if node == None:
+          nullFound = True
+        else:
+          if nullFound:
+            return False
+          else:
+            q.append(node.left)
+            q.append(node.right)
+            
+      return True
+#       while q and q[0] is not None: # add children for all not none nodes
+#         node = q.popleft()
+#         q.append(node.left)
+#         q.append(node.right)
         
-      while q and q[0] == None: # remove none nodes, so if remaining means missing before the most last left 
-        q.popleft()
+#       while q and q[0] == None: # remove none nodes, so if remaining means missing before the most last left 
+#         q.popleft()
               
-      return not q
+#       return not q
         
       
             
