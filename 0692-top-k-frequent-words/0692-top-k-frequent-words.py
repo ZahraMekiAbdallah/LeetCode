8e55dd536b2640ba34cffaf5578b72a644e4d732
ZahraAbdallahMeki <zahrameki18@gmail.com>
2023-05-27 15:11:29 +0300

    Time: 67 ms (45.52%), Space: 16.3 MB (37.88%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -4,29 +4,9 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
       ''' sorted by the frequency then sort the words with the same frequency'''
       dic = Counter(words)
-      dic_vals = defaultdict(list)
       
-      for key, val in dic.items():
-        dic_vals[val].append(key)
-      
-      sorted_dic_vals = sorted(dic_vals.items(), key=lambda x:x[0], reverse=True)
-  
-      flat_sorted_lst = []
-      for key, val in sorted_dic_vals:
-        for item in sorted(val):
-          flat_sorted_lst.append(item)
-          
-      res = []
-      for item in flat_sorted_lst:
-        res.append(item)
-        k-=1
-        if k == 0:
-          break
-          
-      return res
-        
-        
-      
-      
-      
+      sorted_dic = sorted(dic.items(), key=lambda x: (-x[1], x[0])) # sort counter desc then words ascend
+            
+      return [item[0] for item in sorted_dic[:k]]
+                
         
