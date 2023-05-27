b175a18d41256cee0ace1ad81adffca6199d66c3
ZahraAbdallahMeki <zahrameki18@gmail.com>
2023-05-27 19:46:20 +0300

    Time: 2813 ms (25.42%), Space: 20.6 MB (26.07%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,21 +3,23 @@ class Solution:
         res = []
         nums.sort() # Sorted to use 2 pointers and get the sum to decrease or increase l, r
         for i in range(len(nums)-2):
-            if (nums[i] != nums[i-1] and i > 0) or i == 0:# if number duplicated
-                l = i+1
-                r = len(nums)-1
-                while l < r:
-                    if nums[i]+nums[l]+nums[r] > 0:
-                        r-=1
-                        while  i < r < len(nums)-1 and nums[r] == nums[r+1]:
-                          r-=1
-                    else:
-                        if nums[i]+nums[l]+nums[r] == 0:
-                          res.append([nums[i],nums[l],nums[r]])
-                        l+=1
-                        
-                        while i < l < r and nums[l] == nums[l-1]:
-                          l+=1
+            if nums[i] == nums[i-1] and i > 0: # if number duplicated
+              continue
+              
+            l = i+1
+            r = len(nums)-1
+            while l < r:
+                if nums[i]+nums[l]+nums[r] > 0:
+                    r-=1
+                    while  i < r < len(nums)-1 and nums[r] == nums[r+1]:
+                      r-=1
+                else:
+                    if nums[i]+nums[l]+nums[r] == 0:
+                      res.append([nums[i],nums[l],nums[r]])
+                    l+=1
+
+                    while i < l < r and nums[l] == nums[l-1]:
+                      l+=1
                                 
         return res
 
