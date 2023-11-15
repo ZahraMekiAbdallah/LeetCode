a7c5cc743963d7e33c1dc4c2f2303d9285cbe4c8
ZahraAbdallahMeki <zahrameki18@gmail.com>
2023-11-15 23:06:05 +0200

    Time: 25 ms (6.69%), Space: 13.3 MB (27.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,12 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        while i< len(nums):
+        for i in range(len(nums)):
             if nums[i] > nums[-1]:
                 nums[i], nums[-1] = nums[-1], nums[i]
             nums = self.check_prev(nums, i)
-            i+=1
                 
     def check_prev(self, nums, idx):
         for i in range(idx+1):
