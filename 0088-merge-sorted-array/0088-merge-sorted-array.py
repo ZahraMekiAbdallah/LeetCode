852a1532ecd906572e6a5dbec7f7a75e818f1528
ZahraAbdallahMeki <zahrameki18@gmail.com>
2023-11-16 02:17:41 +0200

    Time: 30 ms (5.95%), Space: 13.2 MB (34.48%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -10,24 +10,17 @@ class Solution(object):
         # Edge Case 
         if n == 0:
             return
-
-                
-        i, j = 0, 0
-        while i < len(nums1) and j < n:
-            if nums1[i] > nums2[j]:
-                nums1.pop() # remove zero
-                nums1.insert(i, nums2[j]) #swap
-                nums2[j] = 0
-                j+=1
-            i+=1
+              
+        nums1[m:] = nums2
         
-        n-=1
-        for i in range(len(nums1)-1, -1, -1):
-            if nums1[i] == 0 and nums2[n] != 0:
-                nums1[i] = nums2[n]
-                n-=1
-            else:
-                break
+        k = len(nums1)-1
+        
+        while k > 0:
+            for i in range(k):
+                j = i+1
+                if nums1[i] > nums1[j]:
+                    nums1[i], nums1[j] = nums1[j], nums1[i]
+            k -= 1
 
             
                 
