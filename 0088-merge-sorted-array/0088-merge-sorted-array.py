5aa665460787ff1564e3d4f1a49215cf61ce2ef3
ZahraAbdallahMeki <zahrameki18@gmail.com>
2023-11-16 02:47:05 +0200

    Time: 16 ms (77.86%), Space: 13.2 MB (67.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,20 +7,20 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        # Edge Case 
-        if n == 0:
-            return
-              
-        nums1[m:] = nums2
+        p1, p2 = m-1, n-1
         
-        k = len(nums1)-1
-        
-        while k > 0:
-            for i in range(k):
-                j = i+1
-                if nums1[i] > nums1[j]:
-                    nums1[i], nums1[j] = nums1[j], nums1[i]
-            k -= 1
+        for p in range(m+n-1, -1, -1):
+            if p2 < 0:
+                break
+
+            if p1 >= 0 and nums1[p1] >= nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+
+# O(n+m)
 
             
                 
